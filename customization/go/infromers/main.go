114fbea97913c93808dd65ac157005cb8a7a0af6
mohammad <shalabi67@hotmail.com>
2021-04-23 23:39:45 +0200

    improve informer code

--- customization/go/infromers/main.go
+++ customization/go/infromers/main.go
@@ -1,47 +1,56 @@
 package main
 
 import (
-	"fmt"
 	"flag"
-	"context"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-    "k8s.io/client-go/tools/clientcmd"
-    "k8s.io/client-go/kubernetes"
+	"fmt"
+	"k8s.io/client-go/kubernetes"
+	//	"context"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
 
-    "k8s.io/apimachinery/pkg/util/wait"
-    "k8s.io/client-go/tools/cache"
-    "k8s.io/client-go/informers"
-    "time"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/tools/cache"
+	"time"
 )
 
 func main() {
 	fmt.Println("Get Pods")
 
 	kubeconfig := flag.String("kubeconfig", "/home/mohammad/.kube/config", "kubeconfig file")
-    flag.Parse()
+	flag.Parse()
 
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-    if err != nil {
-        fmt.Printf("The kubeconfig cannot be loaded: %v\n", err)
-        return
-    }
-    flag.Parse()
+	if err != nil {
+		fmt.Printf("The kubeconfig cannot be loaded: %v\n", err)
+		return
+	}
+	flag.Parse()
 
-    config.AcceptContentTypes = "application/vnd.kubernetes.protobuf, application/json"
-    //config.ContentType = "application/vnd.kubernetes.protobuf"
+	config.AcceptContentTypes = "application/vnd.kubernetes.protobuf, application/json"
+	//config.ContentType = "application/vnd.kubernetes.protobuf"
 
-    clientset, err := kubernetes.NewForConfig(config)
+	clientset, err := kubernetes.NewForConfig(config)
 
-    informerFactory := informers.NewSharedInformerFactory(clientset, time.Second*30)
-    podInformer := informerFactory.Core().V1().Pods()
-    podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-        AddFunc: func(new interface{}) {fmt.Println("Add pod event")},
-        UpdateFunc: func(old, new interface{}) {fmt.Println("Update pod event")},
-        DeleteFunc: func(obj interface{}) {fmt.Println("Delete pod event")},
-    })
-    informerFactory.Start(wait.NeverStop)
-    informerFactory.WaitForCacheSync(wait.NeverStop)
+	informerFactory := informers.NewSharedInformerFactory(clientset, time.Second*30)
+	podInformer := informerFactory.Core().V1().Pods()
+	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(new interface{}) {
+			mObj := new.(metav1.Object)
+			fmt.Printf("Add pod event. Pod Name= %s \n", mObj.GetName())
+		},
+		UpdateFunc: func(old, new interface{}) {
+			mObj := new.(metav1.Object)
+			fmt.Printf("Update pod event. Pod Name= %s \n", mObj.GetName())
+		},
+		DeleteFunc: func(obj interface{}) {
+			mObj := obj.(metav1.Object)
+			fmt.Printf("Delete pod event. Pod Name= %s \n", mObj.GetName())
+		},
+	})
+	informerFactory.Start(wait.NeverStop)
+	informerFactory.WaitForCacheSync(wait.NeverStop)
 
-    ln := ""
-    fmt.Scan(&ln)
+	ln := ""
+	fmt.Scan(&ln)
 }
