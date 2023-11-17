e76a6529b10c09f25d6df0b73a8e136a20c0fd9b
mohammad <shalabi67@hotmail.com>
2023-11-17 08:41:45 +0100

    identify hosts using arp and ping

--- networking/linux/hosts.sh
+++ networking/linux/hosts.sh
@@ -11,8 +11,26 @@ read endip
 #startip=1
 #endip=20
 
+
+echo "starting ..."
 for (( i=startip; i<=endip; i++ )) do
   ping -c 1 "$ip3.$i"
 done
+fileName="arp_$(date +%F-%R).txt"
+arp -e | grep -v incomplete > "$fileName"
 
-arp -e | grep -v incomplete > "arp_$(date +%F-%R).txt"
+# update arp cache
+grep -v Address $fileName | awk -F" " {' print $1" "$3 '} > delme.txt
+
+#empty arp
+ip -s neigh flush all
+
+# fill arp
+arp -f delme.txt
+
+rm delme.txt
+rm "$fileName"
+echo "done"
+
+#show arp
+arp -e
